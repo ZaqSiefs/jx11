9cc1071445aa75413b65c6b55a22816c429bff71
Zaq Siefert <zsiefert@gmail.com>
2024-01-02 18:39:44 -0500

    replaced std::sin() with an approximation algorithm

--- JX11/Source/Oscillator.h
+++ JX11/Source/Oscillator.h
@@ -22,14 +22,21 @@ public:
     void reset()
     {
         phase = 0.0f;
+        sin0 = amp * std::sin(phase * TWO_PI);
+        sin1 = amp * std::sin((phase = inc) * TWO_PI);
+        dsin = 2.0f * std::cos(inc * TWO_PI);
     }
     
     float nextSample()
     {
-        phase += inc;
-        if (phase >= 1.0f) {
-            phase -= 1.0f;
-        }
-        return amp * std::sin(TWO_PI * phase);
+        float sinx = dsin * sin0 - sin1;
+        sin1 = sin0;
+        sin0 = sinx;
+        return sinx;
     }
+    
+private:
+    float sin0;
+    float sin1;
+    float dsin;
 };
