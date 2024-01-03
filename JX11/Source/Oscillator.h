767511152753f10f2d5aaec46c2b2bb35c3d16c3
Zaq Siefert <zsiefert@gmail.com>
2024-01-02 19:59:16 -0500

    changed sawtooth osc to BLIT osc

--- JX11/Source/Oscillator.h
+++ JX11/Source/Oscillator.h
@@ -10,48 +10,76 @@
 
 #pragma once
 
+const float PI_OVER_4 = 0.7853981633974483f;
+const float PI = 3.1415926535897932f;
 const float TWO_PI = 6.2831853071795864f;
 
 class Oscillator
 {
 public:
-    float amp;
-    float inc;
-    float phase;
-    
-    float freq;
-    float sampleRate;
-    float phaseBL;
+    float period = 0.0f;
+    float amp = 1.0f;
     
     void reset()
     {
-        phase = 0.0f;
-        phaseBL = -0.5f;
-    }
-    
-    float nextBandlimitedSample()
-    {
-        phaseBL += inc;
-        if (phaseBL >= 1.0f) {
-            phaseBL -= 1.0f;
-        }
+        inc = 0.0f;
+        phase = -0.5f;
         
-        float output = 0.0f;
-        float nyquist = sampleRate / 2.0f;
-        float h = freq;
-        float i = 1.0f;
-        float m = 0.6366197724f; // == 2/pi
-        while (h < nyquist) {
-            output += m * std::sin(TWO_PI * phaseBL * i) / i;
-            h += freq;
-            i += 1.0f;
-            m = -m;
-        }
-        return output;
+        sin0 = 0.0f;
+        sin1 = 0.0f;
+        dsin = 0.0f;
+        
+        dc = 0.0;
     }
     
     float nextSample()
     {
-        return amp * nextBandlimitedSample();
+        float output = 0.0f;
+        
+        phase += inc;
+        
+        if (phase <= PI_OVER_4) {
+            float halfPeriod = period / 2.0f;
+            phaseMax = std::floor(0.5f + halfPeriod) - 0.5f;
+            dc = 0.5f * amp / phaseMax;
+            phaseMax *= PI;
+            
+            inc = phaseMax / halfPeriod;
+            phase = -phase;
+            
+            sin0 = amp * std::sin(phase);
+            sin1 = amp * std::sin(phase - inc);
+            dsin = 2.0f * std::cos(inc);
+            
+            if (phase*phase > 1e-9) {
+                output = sin0 / phase;
+            } else {
+                output = amp;
+            }
+        } else {
+            if (phase > phaseMax) {
+                phase = phaseMax + phaseMax - phase;
+                inc = -inc;
+            }
+            
+            float sinp = dsin * sin0 - sin1;
+            sin1 = sin0;
+            sin0 = sinp;
+            
+            output = sinp / phase;
+        }
+        
+        return output - dc;
     }
+    
+private:
+    float phase;
+    float phaseMax;
+    float inc;
+    
+    float sin0;
+    float sin1;
+    float dsin;
+    
+    float dc;
 };
